2acc09569a87c0595d31828d505d55b9f66295e9
happylemon1 <65505579+happylemon1@users.noreply.github.com>
2025-07-17 19:33:57 -0700

    Updated Encode/Decode with Comments

--- src/problems/Problem0271_EncodeDecodeStirng.java
+++ src/problems/Problem0271_EncodeDecodeStirng.java
@@ -7,6 +7,9 @@ public class Problem0271_EncodeDecodeStirng {
     public static String encode(List<String> strs) {
         StringBuilder res = new StringBuilder();
         for (String s : strs) {
+            // Add the length and a delimiter (#) in front of each word for encoding scheme. 
+            // This is set up this way because a word can be any character (including non alphabet and non alphanumeric which is why we need to store length
+            // before having the delimiter)
             res.append(s.length()).append('#').append(s);
         }
 
@@ -19,17 +22,27 @@ public class Problem0271_EncodeDecodeStirng {
         int j = 0;
         while (i < str.length()) {
             if (str.charAt(i) == '#') {
+                // j still at beginning of length, i reaches delimiter. This is how we extract length
                 int length = Integer.parseInt(str.substring(j, i));
+                // move j past delimiter to beginning of WORD
                 j = i + 1;
+                // i was at delimiter, we move it to start of word + length because substring doesn't include the last char
                 i = j + length;
+                // extract word and add it
                 res.add(str.substring(j, i));
+                // Now, i is onto the next word at the first character of that (length) so we can't forget to move j to i
                 j = i;
             } else {
+                // Progress i continuously until reaching a delimiter.
                 i++;
             }
         }
+        // Return final list of words
         return res;
     }
+    /*
+     * Not too bad of a problem. The decode is much tricker than encode. I was struggling with forgetting to move j to i. Just draw things out and it will be ok.
+     */
     public static void main(String[] args) {
         List<String> strs = new ArrayList<>();
         strs.add("neet");
