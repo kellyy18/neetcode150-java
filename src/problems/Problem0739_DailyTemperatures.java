cb6f4bdc3918941811de79b8d1bb2329b5fa1625
happylemon1 <65505579+happylemon1@users.noreply.github.com>
2025-07-31 23:10:06 -0700

    LC 739 main method\

--- src/problems/Problem0739_DailyTemperatures.java
+++ src/problems/Problem0739_DailyTemperatures.java
@@ -19,4 +19,21 @@ public class Problem0739_DailyTemperatures {
         }
         return res;
     }
+
+    public static void main(String[] args) {
+        Problem0739_DailyTemperatures solver = new Problem0739_DailyTemperatures();
+
+        System.out.println("--- LeetCode Test Case 1 ---");
+        int[] temperatures1 = {30, 38, 30, 36, 35, 40, 28};
+        System.out.println("Input temperatures: " + java.util.Arrays.toString(temperatures1));
+        int[] result1 = solver.dailyTemperatures(temperatures1);
+        System.out.println("Output: " + java.util.Arrays.toString(result1)); // Expected: [1, 4, 1, 2, 1, 0, 0]
+        System.out.println("------------------------------\n");
+
+        System.out.println("--- LeetCode Test Case 2 ---");
+        int[] temperatures2 = {22, 21, 20};
+        System.out.println("Input temperatures: " + java.util.Arrays.toString(temperatures2));
+        int[] result2 = solver.dailyTemperatures(temperatures2);
+        System.out.println("Output: " + java.util.Arrays.toString(result2)); // Expected: [0, 0, 0]
+    }
 }
