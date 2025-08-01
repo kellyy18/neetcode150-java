d1c6fcedd9bacd628f7b308f5b2de51966454052
happylemon1 <65505579+happylemon1@users.noreply.github.com>
2025-07-31 23:06:20 -0700

    LC 74 main method

--- src/problems/Problem0074_Search2DMatrix.java
+++ src/problems/Problem0074_Search2DMatrix.java
@@ -37,4 +37,34 @@ public class Problem0074_Search2DMatrix {
         }
         return false;
     }
+
+    public static void main(String[] args) {
+        Problem0074_Search2DMatrix solver = new Problem0074_Search2DMatrix();
+
+        System.out.println("--- LeetCode Test Case 1 ---");
+        int[][] matrix1 = {
+            {1, 3, 5, 7},
+            {10, 11, 16, 20},
+            {23, 30, 34, 60}
+        };
+        int target1 = 3;
+        System.out.println("Input matrix: " + java.util.Arrays.deepToString(matrix1));
+        System.out.println("Input target: " + target1);
+        boolean result1 = solver.searchMatrix(matrix1, target1);
+        System.out.println("Output: " + result1); // Expected: true
+        System.out.println("------------------------------\n");
+
+        System.out.println("--- LeetCode Test Case 2 ---");
+        int[][] matrix2 = {
+            {1, 3, 5, 7},
+            {10, 11, 16, 20},
+            {23, 30, 34, 60}
+        };
+        int target2 = 13;
+        System.out.println("Input matrix: " + java.util.Arrays.deepToString(matrix2));
+        System.out.println("Input target: " + target2);
+        boolean result2 = solver.searchMatrix(matrix2, target2);
+        System.out.println("Output: " + result2); // Expected: false
+        System.out.println("------------------------------\n");
+    }
 }
