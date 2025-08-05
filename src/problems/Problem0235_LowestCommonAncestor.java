efe3722292e6da121babdac08b22328703364adc
happylemon1 <65505579+happylemon1@users.noreply.github.com>
2025-08-04 20:51:43 -0700

    LC 235

---
+++ src/problems/Problem0235_LowestCommonAncestor.java
@@ -0,0 +1,42 @@
+package problems;
+
+public class Problem0235_LowestCommonAncestor {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (p.val < root.val && q.val < root.val) {
+            return lowestCommonAncestor(root.left, p, q);
+        } else if (p.val > root.val && q.val > root.val) {
+            return lowestCommonAncestor(root.right, p, q);
+        } else {
+            return root; // we found the split point
+        }
+    }
+
+    public static void main(String[] args) {
+        Problem0235_LowestCommonAncestor solution = new Problem0235_LowestCommonAncestor();
+        
+        TreeNode root = new TreeNode(6);
+        TreeNode node2 = new TreeNode(2);
+        TreeNode node8 = new TreeNode(8);
+        TreeNode node0 = new TreeNode(0);
+        TreeNode node4 = new TreeNode(4);
+        TreeNode node7 = new TreeNode(7);
+        TreeNode node9 = new TreeNode(9);
+        TreeNode node3 = new TreeNode(3);
+        TreeNode node5 = new TreeNode(5);
+        
+        root.left = node2;
+        root.right = node8;
+        node2.left = node0;
+        node2.right = node4;
+        node4.left = node3;
+        node4.right = node5;
+        node8.left = node7;
+        node8.right = node9;
+
+        TreeNode p = node2; // Node with value 2
+        TreeNode q = node8; // Node with value 8
+        
+        TreeNode lca = solution.lowestCommonAncestor(root, p, q);
+        System.out.println("Lowest Common Ancestor of " + p.val + " and " + q.val + " is: " + lca.val); // Output: 6
+    }
+}
